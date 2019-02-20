2d64b47f0be7ca92349bf1334da66ab94c1e2501
Grant Neale <grantneale@users.noreply.github.com>
2019-02-20 12:43:50 +1030

    Bindings for Mocha globals.

---
+++ Mocha.fs
@@ -0,0 +1,17 @@
+/// Fable bundings for Mocha, see https://mochajs.org/
+module Fable.Import.Mocha
+
+open Fable.Core
+
+let [<Global>] describe (title : string) (f : unit -> unit) = jsNative
+let [<Global>] xdescribe (title : string) (f : unit -> unit) = jsNative
+let [<Global>] context (title : string) (f : unit -> unit) = jsNative
+let [<Global>] xcontext (title : string) (f : unit -> unit) = jsNative
+let [<Global>] it (msg : string) (f : unit -> unit) = jsNative
+let [<Global>] xit (msg : string) (f : unit -> unit) = jsNative
+let [<Global>] specify (msg : string) (f : unit -> unit) = jsNative
+let [<Global>] xspecify (msg : string) (f : unit -> unit) = jsNative
+let [<Global>] before (name : string) (f : unit -> unit) = jsNative
+let [<Global>] beforeEach (name : string) (f : unit -> unit) = jsNative
+let [<Global>] after (name : string) (f : unit -> unit) = jsNative
+let [<Global>] afterEach (name : string) (f : unit -> unit) = jsNative
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # fable-mocha
-Fable bindings for Mocha
+Fable bindings for [Mocha](https://mochajs.org/).
+
+## Build Configuration
+
+See https://harry.vangberg.name/unit-testing-fable-with-mocha
+
+## Example
+
+```
+open Mocha
+open Matchers
+
+
+/// Assert that the expected and actual values are equal
+let assertEqual expected actual: unit =
+    Assert.AreEqual(actual, expected)
+
+/// Assert that the expected and actual values are not equal
+let assertNotEqual expected actual: unit =
+    Assert.NotEqual(actual, expected)
+
+describe "test suite" (fun _ ->
+    it "should do something poorly" (fun _ ->
+        assertEqual 1 2
+    )
+    it "should do something well" (fun _ ->
+        assertEqual 2 2
+    )
+    it "should do something else well" (fun _ ->
+        assertNotEqual 23 65
+    )
+)
+|> ignore
+```
+
+
